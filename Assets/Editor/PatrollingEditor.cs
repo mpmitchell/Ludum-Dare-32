4a883dad364a26ea6ff36cbac03463781d0a21e7
Matthew Mitchell <matt.mancer@gmail.com>
2015-04-18 19:01:30 +0100

    Patrolling Enemy added

---
+++ Assets/Editor/PatrollingEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Patrolling))]
+public class PatrollingEditor : Editor {
+	void OnSceneGUI() {
+		Patrolling patrolling = (Patrolling) target;
+
+		Handles.color = Color.green;
+		patrolling.startPoint = Handles.Slider(patrolling.startPoint, -Vector3.right, 0.3f, Handles.CubeCap, 0.0f);
+
+		Handles.color = Color.red;
+		patrolling.endPoint = Handles.Slider(patrolling.endPoint, Vector3.right, 0.3f, Handles.CubeCap, 0.0f);
+
+		if (GUI.changed) {
+			EditorUtility.SetDirty(target);
+		}
+	}
+}
---
+++ Assets/Enemies/Scripts/Patrolling.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrolling : MonoBehaviour {
+	public float speed = 5.0f;
+	public bool isMovingLeft = true;
+
+	public Vector3 startPoint;
+	public Vector3 endPoint;
+
+	float t;
+
+	void Start() {
+		if (startPoint.x > endPoint.x) {
+			Vector3 swap = startPoint;
+			startPoint = endPoint;
+			endPoint = swap;
+		}
+
+		t = (transform.position.x - startPoint.x) / endPoint.x;
+	}
+
+	void Update() {
+		if (isMovingLeft) {
+			t -= speed * Time.deltaTime / Mathf.Abs(endPoint.x - startPoint.x);
+			if (t <= 0.0f) {
+				t = 0.0f - t;
+				isMovingLeft = false;
+			}
+		} else {
+			t += speed * Time.deltaTime / Mathf.Abs(endPoint.x - startPoint.x);
+			if (t >= 1.0f) {
+				t = 1.0f - (t - 1.0f);
+				isMovingLeft = true;
+			}
+		}
+
+		transform.position = new Vector3(startPoint.x + t * endPoint.x, transform.position.y, transform.position.z);
+	}
+}
