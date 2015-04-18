f371470b30fae71567773e7275b2f35e43597d5c
ckinne1771 <ckinney1771@gmail.com>
2015-04-18 19:12:22 +0100

    Stopped player from moving left & right midair

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ public class PlayerController : MonoBehaviour {
 
 	private bool moveRight = false;
 	private bool moveLeft = false;
+
+	private bool moveDisabled = true;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.GetKeyDown(KeyCode.D))
+		if (Input.GetKeyDown(KeyCode.D) && moveDisabled==false)
 		{
 			moveRight = true;
 
@@ -23,7 +26,7 @@ public class PlayerController : MonoBehaviour {
 				moveRight = false;
 		}
 		
-		if (Input.GetKeyDown (KeyCode.A))
+		if (Input.GetKeyDown (KeyCode.A) && moveDisabled==false) 
 		{
 			moveLeft = true;
 
@@ -36,13 +39,34 @@ public class PlayerController : MonoBehaviour {
 
 		if(moveRight ==true)
 		{
-			transform.Translate(11.0f*Time.deltaTime, 0.0f, 0.0f);
+			transform.Translate(7.0f*Time.deltaTime, 0.0f, 0.0f);
 		}
 		if(moveLeft==true)
 		{
-			transform.Translate(-11.0f*Time.deltaTime, 0.0f, 0.0f);
+			transform.Translate(-7.0f*Time.deltaTime, 0.0f, 0.0f);
 		}
 
+
 	} 
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log("Working");
+		if(other.gameObject.tag == "World")
+		{
+			moveDisabled = false;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if(other.gameObject.tag == "World")
+		{
+			moveDisabled = true;
+		}
+	}
+
+
 }
+
  
