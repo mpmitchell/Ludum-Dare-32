1bb0e1298f00e4265518a1769fec3132cd68edca
Matthew Mitchell <matt.mancer@gmail.com>
2015-04-18 16:22:08 +0100

    Base Pickup added

---
+++ Assets/Pickups/Scripts/BasePickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BasePickup : MonoBehaviour {
+	public bool rotate = true;
+	public float rotationalSpeed = 90.0f;
+
+	void Update() {
+		transform.Rotate(Vector3.up * rotationalSpeed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.tag.Equals("Player")) {
+			Activate(collision.collider.gameObject);
+		}
+	}
+
+	protected abstract void Activate(GameObject player);
+}
---
+++ Assets/Pickups/Scripts/TestPickup.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPickup : BasePickup {
+	protected override void Activate(GameObject player) {
+		//
+	}
+}
