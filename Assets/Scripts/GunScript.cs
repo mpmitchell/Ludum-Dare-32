8c0952529d890c0e6ccbe5165b575f21630fc431
ckinne1771 <ckinney1771@gmail.com>
2015-04-18 19:20:17 +0100

    Timer between shots added
    Set at 1 sec between shots currently. TODO: Fix bugs and ensure weapon
    can only be fired at obstacles within range

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GunScript : MonoBehaviour {
 
 	public GameObject player;
+	private float timer = 0;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
@@ -11,6 +12,7 @@ public class GunScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		timer-= Time.deltaTime;
 		Vector3 mPosition = Camera.main.WorldToScreenPoint (transform.position);
 		Vector3 mDirection = Input.mousePosition - mPosition;
 		float angle = Mathf.Atan2 (mDirection.y, mDirection.x) * Mathf.Rad2Deg;
@@ -18,11 +20,16 @@ public class GunScript : MonoBehaviour {
 
 		if (Input.GetMouseButtonDown (0))
 		{
+			if (timer<=0){
+
 			player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 			mPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			Vector2 ForceVector =(mPosition - player.transform.position).normalized;
 			Debug.Log (ForceVector.normalized*50*-1);
 			player.GetComponent<Rigidbody2D>().AddForce(ForceVector.normalized*50*-1);
+			
+				timer = 1;
+			}
 		} 
 	}
 }
