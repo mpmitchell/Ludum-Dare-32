d18cb1e667d356da2f4041feb1b6c4717ad51c29
ckinne1771 <ckinney1771@gmail.com>
2015-04-18 15:45:55 +0100

    Gun rotates with mouse movement
    TODO: Add shockwave

---
+++ New Unity Project 1/Assets/Scripts/GunScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mPosition = Camera.main.WorldToScreenPoint (transform.position);
+		Vector3 mDirection = Input.mousePosition - mPosition;
+		float angle = Mathf.Atan2 (mDirection.y, mDirection.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+}
+ 
