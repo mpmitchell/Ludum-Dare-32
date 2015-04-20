e255e82bfa3b9254255fceb47ab4c1a08f6c2c27
Matthew Mitchell <matt.mancer@gmail.com>
2015-04-20 18:20:19 +0100

    Goal & level complete

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+	[SerializeField] GameObject healthbar;
+	[SerializeField] GameObject levelComplete;
+	[SerializeField] GameObject pauseButton;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.tag.Equals("Player")
+			&& collision.contacts[0].normal == -Vector2.up) {
+			Time.timeScale = 0.0f;
+			levelComplete.SetActive(true);
+			healthbar.SetActive(false);
+			pauseButton.SetActive(false);
+		}
+	}
+}
