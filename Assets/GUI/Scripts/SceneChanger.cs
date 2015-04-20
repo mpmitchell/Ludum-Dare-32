b669002a17602bdefd9af11ea466cad7ad75ee28
Matthew Mitchell <matt.mancer@gmail.com>
2015-04-20 12:31:29 +0100

    Main menu and level select scenes

---
+++ Assets/GUI/Scripts/SceneChanger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+	public void ChangeScene(string scene) {
+		Application.LoadLevel(scene);
+	}
+
+	public void Exit() {
+		Application.Quit();
+	}
+}
