fd655a40efcc2012447a526a07ca106cfb73a4b6
Matthew Mitchell <matt.mancer@gmail.com>
2015-04-20 11:35:33 +0100

    Control tweaks

--- Assets/Player/Scripts/GunScript.cs
+++ Assets/Player/Scripts/GunScript.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class GunScript : MonoBehaviour {
 	[SerializeField] float firePeroid = 1.0f;
 	[SerializeField] float maxDistance = 10.0f;
-	[SerializeField] float maxForce = 50.0f;
+	[SerializeField] float force = 80.0f;
 
 	float timer = 0.0f;
+
+	new Rigidbody2D rigidbody;
+
+	void Start() {
+		rigidbody = ServiceLocator.player.GetComponent<Rigidbody2D>();
+	}
 
 	void Update() {
 		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -19,8 +25,8 @@ public class GunScript : MonoBehaviour {
 			if (Input.GetButtonDown("Fire1")) {
 				RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 100, LayerMask.GetMask("World"));
 				if (hit.distance <= maxDistance) {
-					Vector2 forceVector = (mousePosition - ServiceLocator.player.transform.position).normalized;
-					ServiceLocator.player.GetComponent<Rigidbody2D>().AddForce(-forceVector * maxForce * maxDistance / hit.distance);
+					Vector2 forceVector = mousePosition - ServiceLocator.player.transform.position;
+					rigidbody.AddForce(-forceVector.normalized * force * Time.deltaTime, ForceMode2D.Impulse);
 
 					timer = firePeroid;
 				}
