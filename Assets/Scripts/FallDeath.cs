d31a8604aba153bbc1951715c6316d5770a32cc8
Matthew Mitchell <matt.mancer@gmail.com>
2015-04-20 16:45:57 +0100

    Falling death

---
+++ Assets/Scripts/FallDeath.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallDeath : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.tag.Equals("Player")) {
+			collision.collider.GetComponent<Health>().health = 0;
+		}
+	}
+}
